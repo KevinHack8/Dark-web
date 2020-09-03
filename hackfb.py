c1edc729bea38af8cedc267ba47f7a2f97160cbd
KevinHack8 <70560888+KevinHack8@users.noreply.github.com>
2020-09-03 11:09:36 +0700

    Update hackfb.py

--- hackfb.py
+++ hackfb.py
@@ -16,7 +16,7 @@ def restart():
 user = raw_input('ID: ')
 import getpass
 sandi = raw_input('Password: ')
-if sandi == 'indoxploit' and user == 'Borot':
+if sandi == 'KevinHack' and user == 'Subscribe':
     print 'Anda Telah Login'
     sys.exit
 else:
